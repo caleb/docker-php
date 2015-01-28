90bb2defe0b83749fc8f295f78bbd9d9ae19bd1e
Caleb Land <caleb@land.fm>
2015-01-28 15:33:57 -0500

    remove the bin folder

--- 5.6/fpm/wordpress/build.sh
+++ 5.6/fpm/wordpress/build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-if [ ! -f bin/mo ]; then
-  curl https://raw.githubusercontent.com/caleb/mo/master/mo > bin/mo
-  chmod +x bin/mo
-fi
-
 docker build -t docker.rodeopartners.com/php:5.6-fpm-wordpress .

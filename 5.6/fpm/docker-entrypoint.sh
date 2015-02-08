309b01aa7478c9ab94d5c2d28de9fcc502768bbf
Caleb Land <caleb@land.fm>
2015-02-08 16:40:10 -0500

    Allow the creation of links (for uploads etc)

--- 5.6/fpm/docker-entrypoint.sh
+++ 5.6/fpm/docker-entrypoint.sh
@@ -12,7 +12,26 @@ export PHP_FPM_MAX_SPARE_SERVERS
 : ${PHP_FPM_MAX_CHILDREN:=5}
 : ${PHP_FPM_START_SERVERS=2}
 : ${PHP_FPM_MIN_SPARE_SERVERS:=1}
-: ${PHP_FPM_MAX_SPARE_SERVERS=3}
+: ${PHP_FPM_MAX_SPARE_SERVERS:=3}
+
+if [ -n "${PHP_FPM_SHARED_LINK}" ]; then
+  PHP_FPM_SHARED_LINK_DEFAULT="${PHP_FPM_SHARED_LINK}"
+fi
+
+# Link shared directories (like uploads)
+for link_var in ${!PHP_FPM_SHARED_LINK_*}; do
+  link="${!link_var}"
+  if [ -n "${link}" ]; then
+    from="${link%%:*}"
+    to="${link#*:}"
+    if [ -z "${from}" ] || [ -z "${to}" ]; then
+      echo "A link must be in the form <from>:<to>"
+      exit 1
+    else
+      ln -f -s -T "${from}" "${to}"
+    fi
+  fi
+done
 
 # Fill out the templates
 for f in /usr/local/etc/**/*.mo; do

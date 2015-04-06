75d59ba7927d9ef3b03299e7c16b76a2b6ddbe6c
Caleb Land <caleb@land.fm>
2015-04-06 13:10:22 -0400

    Update build system

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Building php:5.6-fpm"
-docker build -t docker.rodeopartners.com/php:5.6-fpm 5.6/fpm
+cd 5.6/fpm
+./build.sh
+cd ../..
+docker tag -f docker.rodeopartners.com/php:5.6-fpm docker.rodeopartners.com/php:latest-fpm
 
 echo "Building php:5.6-fpm-wordpress"
-docker build -t docker.rodeopartners.com/php:5.6-fpm-wordpress 5.6/fpm/wordpress
+cd 5.6/fpm/wordpress
+./build.sh
+cd ../../..
+docker tag -f docker.rodeopartners.com/php:5.6-fpm-wordpress docker.rodeopartners.com/php:latest-fpm-wordpress
--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-docker push docker.rodeopartners.com/php:5.6-fpm
-docker push docker.rodeopartners.com/php:5.6-fpm-wordpress
+5.6/fpm/push.sh
+docker push docker.rodeopartners.com/php:latest-fpm
+
+5.6/fpm/wordpress/push.sh
+docker push docker.rodeopartners.com/php:latest-fpm-wordpress
